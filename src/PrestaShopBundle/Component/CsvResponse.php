03fa5522eeb845e6a83ba6ed5cb702b2bfc13b48
Peter Schaeffer <peter.schaeffer@shoprunners.de>
2022-03-30 08:30:42 +0200

    Adds the possibility to skip header row
    If somebody does not want to add the header row, skip it.

--- src/PrestaShopBundle/Component/CsvResponse.php
+++ src/PrestaShopBundle/Component/CsvResponse.php
@@ -1,11 +1,12 @@
 <?php
 /**
- * 2007-2018 PrestaShop.
+ * Copyright since 2007 PrestaShop SA and Contributors
+ * PrestaShop is an International Registered Trademark & Property of PrestaShop SA
  *
  * NOTICE OF LICENSE
  *
  * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
+ * that is bundled with this package in the file LICENSE.md.
  * It is also available through the world-wide-web at this URL:
  * https://opensource.org/licenses/OSL-3.0
  * If you did not receive a copy of the license and are unable to
@@ -16,12 +17,11 @@
  *
  * Do not edit or add to this file if you wish to upgrade PrestaShop to newer
  * versions in the future. If you wish to customize PrestaShop for your
- * needs please refer to http://www.prestashop.com for more information.
+ * needs please refer to https://devdocs.prestashop.com/ for more information.
  *
- * @author    PrestaShop SA <contact@prestashop.com>
- * @copyright 2007-2018 PrestaShop SA
+ * @author    PrestaShop SA and Contributors <contact@prestashop.com>
+ * @copyright Since 2007 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/OSL-3.0 Open Software License (OSL 3.0)
- * International Registered Trademark & Property of PrestaShop SA
  */
 
 namespace PrestaShopBundle\Component;
@@ -32,13 +32,13 @@ use Symfony\Component\HttpFoundation\StreamedResponse;
 class CsvResponse extends StreamedResponse
 {
     // Mode used to paginate page per page, 1/100, 2/100, 3/000, etc
-    const MODE_PAGINATION = 1;
+    public const MODE_PAGINATION = 1;
 
     // Mode used to paginate by offset, 1/100, 100/100, 200/100, etc (like MySql limit)
-    const MODE_OFFSET = 2;
+    public const MODE_OFFSET = 2;
 
     /**
-     * @var array() CSV content
+     * @var array|callable CSV content
      */
     private $data;
 
@@ -50,7 +50,7 @@ class CsvResponse extends StreamedResponse
     /**
      * @var array
      */
-    private $headersData = array();
+    private $headersData = [];
 
     /**
      * @var int, self::MODE_PAGINATION by default
@@ -58,7 +58,7 @@ class CsvResponse extends StreamedResponse
     private $modeType = self::MODE_PAGINATION;
 
     /**
-     * @var null|int
+     * @var int|null
      */
     private $start = null;
 
@@ -68,18 +68,23 @@ class CsvResponse extends StreamedResponse
     private $limit = 1000;
 
     /**
+     * @var boolean exportHeader
+     */
+    private $exportHeader = true;
+
+    /**
      * Constructor.
      *
      * @param callable|null $callback A valid PHP callback or null to set it later
      * @param int $status The response status code
      * @param array $headers An array of response headers
      */
-    public function __construct($callback = null, $status = 200, $headers = array())
+    public function __construct($callback = null, $status = 200, $headers = [])
     {
         parent::__construct($callback, $status, $headers);
 
-        if (is_null($callback)) {
-            $this->setCallback(array($this, 'processData'));
+        if (null === $callback) {
+            $this->setCallback([$this, 'processData']);
         }
 
         $this->setFileName('export_' . date('Y-m-d_His') . '.csv');
@@ -111,7 +116,7 @@ class CsvResponse extends StreamedResponse
     }
 
     /**
-     * @param $modeType int
+     * @param int $modeType
      *
      * @return $this
      */
@@ -123,7 +128,7 @@ class CsvResponse extends StreamedResponse
     }
 
     /**
-     * @param $start int
+     * @param int $start
      *
      * @return $this
      */
@@ -135,7 +140,7 @@ class CsvResponse extends StreamedResponse
     }
 
     /**
-     * @param $limit int
+     * @param int $limit
      *
      * @return $this
      */
@@ -162,6 +167,18 @@ class CsvResponse extends StreamedResponse
             $this->fileName
         );
         $this->headers->set('Content-Disposition', $disposition);
+
+        return $this;
+    }
+
+    /**
+     * @param boolean $exportHeader
+     *
+     * @return $this
+     */
+    public function setExportHeader($exportHeader)
+    {
+        $this->exportHeader = (boolean) $exportHeader;
 
         return $this;
     }
@@ -196,7 +213,10 @@ class CsvResponse extends StreamedResponse
     private function processDataArray()
     {
         $handle = tmpfile();
-        fputcsv($handle, $this->headersData, ';');
+
+        if ($this->exportHeader) {
+            fputcsv($handle, $this->headersData, ';');
+        }
 
         foreach ($this->data as $line) {
             fputcsv($handle, $line, ';');
@@ -211,10 +231,13 @@ class CsvResponse extends StreamedResponse
     private function processDataCallback()
     {
         $handle = tmpfile();
-        fputcsv($handle, $this->headersData, ';');
+
+        if ($this->exportHeader) {
+            fputcsv($handle, $this->headersData, ';');
+        }
 
         do {
-            $data = call_user_func_array($this->data, array($this->start, $this->limit));
+            $data = call_user_func_array($this->data, [$this->start, $this->limit]);
 
             $count = count($data);
             if ($count === 0) {
@@ -222,7 +245,7 @@ class CsvResponse extends StreamedResponse
             }
 
             foreach ($data as $line) {
-                $lineData = array();
+                $lineData = [];
 
                 foreach (array_keys($this->headersData) as $column) {
                     if (array_key_exists($column, $line)) {
@@ -280,7 +303,7 @@ class CsvResponse extends StreamedResponse
     }
 
     /**
-     * @param $handle, file pointer
+     * @param resource $handle file pointer
      */
     private function dumpFile($handle)
     {
